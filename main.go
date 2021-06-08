f486951dac297801825de5f3bcc9210e66310716
Ilya Voronin <ivoronin@gmail.com>
2021-06-08 22:06:33 +0000

    Bump version to 0.1.0

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	Version           = "0.0.2"
+	Version           = "0.1.0"
 	VersionPrerelease = ""
 	PluginVersion     = version.InitializePluginVersion(Version, VersionPrerelease)
 )
