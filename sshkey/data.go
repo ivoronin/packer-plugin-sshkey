f34d27a37325e4cff9749c41eb32c6acfb0f6723
Ilya Voronin <ivoronin@gmail.com>
2022-03-06 14:21:15 +0000

    fix

--- sshkey/data.go
+++ sshkey/data.go
@@ -49,7 +49,7 @@ func (d *Datasource) OutputSpec() hcldec.ObjectSpec {
 }
 
 func (d *Datasource) Execute() (cty.Value, error) {
-	var privateKey *ed25519.PrivateKey
+	var privateKey ed25519.PrivateKey
 	var err error
 
 	keyName := d.config.Name
@@ -99,23 +99,22 @@ func (d *Datasource) Execute() (cty.Value, error) {
 	return hcl2helper.HCL2ValueFromConfig(output, d.OutputSpec()), nil
 }
 
-func generatePrivateKey() (*ed25519.PrivateKey, error) {
+func generatePrivateKey() (ed25519.PrivateKey, error) {
 	_, privateKey, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		return nil, err
 	}
 
-	return &privateKey, nil
+	return privateKey, nil
 }
 
-func encodePrivateKeyToPEM(privateKey *ed25519.PrivateKey) ([]byte, error) {
-	privDER, err := x509.MarshalPKCS8PrivateKey(*privateKey)
+func encodePrivateKeyToPEM(privateKey ed25519.PrivateKey) ([]byte, error) {
+	privDER, err := x509.MarshalPKCS8PrivateKey(privateKey)
 	if err != nil {
 		return nil, err
 	}
 	privBlock := pem.Block{
 		Type:    "PRIVATE KEY",
-		Headers: nil,
 		Bytes:   privDER,
 	}
 
@@ -124,7 +123,7 @@ func encodePrivateKeyToPEM(privateKey *ed25519.PrivateKey) ([]byte, error) {
 	return privatePEM, nil
 }
 
-func decodePrivateKeyFromPEM(privateKeyPEM []byte) (*ed25519.PrivateKey, error) {
+func decodePrivateKeyFromPEM(privateKeyPEM []byte) (ed25519.PrivateKey, error) {
 	block, _ := pem.Decode(privateKeyPEM)
 	if block == nil || block.Type != "PRIVATE KEY" {
 		return nil, errors.New("unable to decode PEM")
@@ -135,7 +134,7 @@ func decodePrivateKeyFromPEM(privateKeyPEM []byte) (*ed25519.PrivateKey, error) 
 		return nil, err
 	}
 
-	return privateKey.(*ed25519.PrivateKey), nil
+	return privateKey.(ed25519.PrivateKey), nil
 }
 
 func generatePublicKeyString(publicKey crypto.PublicKey) (string, error) {
