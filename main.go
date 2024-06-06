dfe09c254683e0c2a8c216a27487de062e394efe
Ilya Voronin <ivoronin@gmail.com>
2024-06-06 21:49:52 +0200

    bumped Go version

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	Version           = "1.2.0"
+	Version           = "1.2.1"
 	VersionPrerelease = ""
 	PluginVersion     = version.InitializePluginVersion(Version, VersionPrerelease)
 )
